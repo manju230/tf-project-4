2f2bbc9f2a29faa2404b26464611f9f97d3dd7d6
unknown <dvaram63@gmail.com>
2026-09-25 05:50:17 +0000

    s3

--- dev/s3/backend.tf
+++ dev/s3/backend.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "s3" {
    bucket = "terraform-s3-backend-prod-tfstate-us"
-   key = "tf-project-3/s3.tfstate"
+   key = "tf-project-4/s3.tfstate"
    region = "us-east-1"
    encrypt = true
  }
